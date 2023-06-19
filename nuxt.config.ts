6e12cb0d50d913d20418ffae1d59ae9721f2a8ba
Anish Ghimire <anishghimire862@gmail.com>
2023-06-19 14:35:38 +0545

    add apiSecret

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 export default defineNuxtConfig({
   runtimeConfig: {
+    apiSecret: '123456',
     public: {
       appName: process.env.NUXT_PUBLIC_APP_NAME,
       appUrl: process.env.NUXT_PUBLIC_APP_URL,
